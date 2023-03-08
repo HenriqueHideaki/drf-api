61b2a70628dead8217723a97db1c0ec4eed429fd
Henrique Hideaki <hideakikoga.hk@gmail.com>
2023-03-08 19:27:42 -0300

    testes

--- agenda/tests.py
+++ agenda/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from rest_framework.test import APITestCase
+import json
 # Create your tests here.
+class TestListagemAgendamentos(APITestCase):
+    def test_listagem_vazia(self):
+        response = self.client.get("/api/agendamentos/")
+        data =  json.loads(response.content)
+        self.assertEqual(data, [])
--- agenda/views.py
+++ agenda/views.py
@@ -3,29 +3,30 @@ from agenda.serializers import AgendamentoSerializer
 from django.http import JsonResponse 
 from agenda.models import Agendamento
 from rest_framework.decorators import api_view
+from rest_framework.response import Response 
 
 
-
-@api_view(http_method_names=["GET", "PUT"])
+@api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def agendamento_detail(request, id):
     
     if request.method == "GET":
         obj = get_object_or_404(Agendamento, id=id)
         serializer = AgendamentoSerializer(obj)
         return JsonResponse(serializer.data)
-    if request.method == "PUT":
+    
+    if request.method == "PATCH":
         obj = get_object_or_404(Agendamento, id=id)
-        serializer = AgendamentoSerializer(data=request.data)
+        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)
         if serializer.is_valid():
             v_data = serializer.validated_data
-            obj.data_horario = v_data.get("data_horario", obj.data_horario)
-            obj.nome_cliente = v_data.get("nome_cliente", obj.nome_cliente)
-            obj.email_cliente = v_data.get("email_cliente", obj.email_cliente)
-            obj.telefone_cliente = v_data.get("telefone_cliente", obj.telefone_cliente)
-            obj.save()
-            return JsonResponse(serializer.data, status=200)
+            serializer.save()
+            return JsonResponse(v_data, status=200)
         return JsonResponse(serializer.errors, status=400)
     
+    if request.method == "DELETE":
+        obj = get_object_or_404(Agendamento, id=id)     
+        obj.delete()   
+        return Response(status=204)
     
 @api_view(http_method_names=["GET", "POST"])   
 def agendamento_list(request):
@@ -37,12 +38,6 @@ def agendamento_list(request):
         data = request.data
         serializer = AgendamentoSerializer(data=data)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            Agendamento.objects.create(
-                data_horario = validated_data["data_horario"],
-                nome_cliente = validated_data["nome_cliente"],
-                email_cliente = validated_data["email_cliente"],
-                telefone_cliente = validated_data["telefone_cliente"]
-            )
+            serializer.save()
             return JsonResponse(serializer.data,status=201)
         return JsonResponse(serializer.erros, status=400)
